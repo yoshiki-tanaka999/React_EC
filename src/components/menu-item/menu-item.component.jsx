1200e27b9cab0f3752bc94b238e2df2010aaac25
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-16 01:10:05 +0900

    component化と、スタイル修正

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const menuItem = ({ title, imageUrl, size }) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }}
+        className={`${size} menu-item`}
+    >
+        <div className='content'>
+            <h1 className='title'>{ title }</h1>                
+            <span className='sub-title'>SHOP NOW</span>                
+        </div>
+    </div>
+);
+
+export default menuItem;
