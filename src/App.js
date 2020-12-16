9629f83ab81a31cb4243405fa1d1f0a33bc43c89
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-17 01:01:19 +0900

    SHOP_DATAのレンダリング実装。

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
         {/* ベースとなるルーティング */}
         {/* exactがないと、「/hats」の時、「/」も含まれたルーティングになってしまう */}
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ ShopPage } />
       </Switch>
 
     </div>
