12fd841c71cf05dd4cbf61a137d6d6891db92c6f
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-22 13:07:02 +0900

    メール認証、Firebaseの接続修正

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 class App extends React.Component {
   constructor() {
@@ -25,10 +25,24 @@ class App extends React.Component {
   // でも、これって、reder()では、情報を取得していない(別の問題？)だから、悪くはないのかな？
   // 認証ができれいれば、実行。currentUserのデータを取得。
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+            
+            console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth});
+    });
   }
 
   // マウントの解除(例えば、componentDidMountでの設定を解除できる)。
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,32 @@ const  config = {
     messagingSenderId: "1040453813537",
     appId: "1:1040453813537:web:e96022e8705f7d83da2a60"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    // snapShotが存在しなかった時(ユーザー情報が登録されていない)
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error) {
+            console.log('error creating user', error.messsage);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
