0350ff12a5cdeae722e3c5c02f060e7b96feab30
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-23 19:12:34 +0900

    Reduxの導入と、認証部分をReduxに置き換える

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
@@ -7,17 +8,9 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+import { setCurrentUser } from './redux/user/user-actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
-
   unsubscribeFromAuth = null;
 
   // componentDidMount内でsetStateを使わないほうが良いらしい。
@@ -25,23 +18,20 @@ class App extends React.Component {
   // でも、これって、reder()では、情報を取得していない(別の問題？)だから、悪くはないのかな？
   // 認証ができれいれば、実行。currentUserのデータを取得。
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
+          setCurrentUser({
                 id: snapShot.id,
                 ...snapShot.data()
-              }
             });
-            
-            console.log(this.state);
         });
       }
-      this.setState({ currentUser: userAuth});
+      setCurrentUser({ userAuth });
     });
   }
 
@@ -53,7 +43,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           {/* ベースとなるルーティング */}
           {/* exactがないと、「/hats」の時、「/」も含まれたルーティングになってしまう */}
@@ -65,7 +55,12 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
 
-export default App;
+export default connect(
+  null, 
+  mapDispatchToProps
+  )(App);
