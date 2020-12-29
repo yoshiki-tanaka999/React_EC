fd5afd13ca19a785c83d721e3f0ae8683901632d
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-30 00:25:40 +0900

    cart.utils.jsの設定(カートにボタンをクリックした数が加算される処理)を実装

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button 
-        className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} 
-        { ...otherProps }
-    >
-        { children }
-    </button>
+const CustomButton = ({ 
+        children, 
+        isGoogleSignIn, 
+        inverted, 
+        ...otherProps 
+    }) => (
+        <button 
+            className={
+                `custom-button ${inverted ? 'inverted' : '' } 
+                ${ isGoogleSignIn ? 'google-sign-in' : '' }`} 
+            { ...otherProps }
+        >
+            { children }
+        </button>
 );
 export default CustomButton;
