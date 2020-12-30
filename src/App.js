e02564001248b4cbe6a8870b376f1bddcae5eafe
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-30 21:50:38 +0900

    checkoutPage, checkout-itemコンポーネントの作成。

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component.jsx';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+
 import { setCurrentUser } from './redux/user/user-actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   // componentDidMount内でsetStateを使わないほうが良いらしい。
   // 使うなら、constructor内で走らせたほうが吉。
-  // でも、これって、reder()では、情報を取得していない(別の問題？)だから、悪くはないのかな？
+  // でも、これって、render()では、情報を取得していない(別の問題？)だから、悪くはないのかな？
   // 認証ができれいれば、実行。currentUserのデータを取得。
   componentDidMount() {
     const { setCurrentUser } = this.props;
@@ -50,6 +56,7 @@ class App extends React.Component {
           {/* exactがないと、「/hats」の時、「/」も含まれたルーティングになってしまう */}
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ ShopPage } />
+          <Route exact path='/checkout' component={ CheckoutPage } />
           <Route 
             exact 
             path='/signin' 
@@ -67,8 +74,8 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils.js';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -31,11 +34,11 @@ const Header = ({ currentUser, hidden }) => (
             <CartIcon />
         </div>
         { hidden ? null : <CartDropDown /> }
-
     </div>
 );
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 export default connect(mapStateToProps)(Header);
